89f24a615a1cff766e75504e7aea001954688bb7
lsterick <laurastericker@gmail.com>
2021-03-29 15:59:13 -0400

    Day 11 2020

--- src/day11.ts
+++ src/day11.ts
@@ -1,17 +1,87 @@
-export default function (input: string): string {
-  const adapters: Array<number> = input.split("\n").map((i) => parseInt(i)).sort(function (a, b) {
-    return a - b;
-  });
-  console.log(JSON.stringify(adapters))
-  let countOfOptions = 0;
+function isSightlineOccupied(i: number, j: number, currentSeatsOccupied: Array<Array<String>>, nextI: (i: number) => number, nextJ: (j:number) => number ): boolean {
+  i = nextI(i);
+  j = nextJ(j);
 
-  for(let i = 1; i < adapters.length - 1; i++){
-    if(adapters[i] - adapters[i-1] === 1){
-      
-    } 
-
-    // If the gap was 3 to start with, there's nothing interesting to do.
+  if(i === -1 || j === -1 || i === currentSeatsOccupied.length || j === currentSeatsOccupied[i].length || currentSeatsOccupied[i][j] === "L"){
+    return false;
   }
 
-  return (countOfOptions).toString();
+  if(currentSeatsOccupied[i][j] === "#"){
+    return true;
+  }
+
+  return isSightlineOccupied(i, j, currentSeatsOccupied, nextI, nextJ);
+}
+
+function countAdjacentOccupiedSeats (i: number, j: number, currentSeatsOccupied: Array<Array<String>>): number {
+  let count = 0;
+
+  if (isSightlineOccupied(i, j, currentSeatsOccupied, (i) => i - 1, (j) => j - 1)){
+    count++;
+  }
+  if(isSightlineOccupied(i, j, currentSeatsOccupied, (i) => i - 1, (j) => j)){
+    count++;
+  }
+  if(isSightlineOccupied(i, j, currentSeatsOccupied, (i) => i - 1, (j) => j + 1)){
+    count++;
+  }
+  if(isSightlineOccupied(i, j, currentSeatsOccupied, (i) => i, (j) => j - 1)){
+    count++;
+  }
+  if(isSightlineOccupied(i, j, currentSeatsOccupied, (i) => i, (j) => j + 1)){
+    count++;
+  }
+  if(isSightlineOccupied(i, j, currentSeatsOccupied, (i) => i + 1, (j) => j - 1)){
+    count++;
+  }
+  if(isSightlineOccupied(i, j, currentSeatsOccupied, (i) => i + 1, (j) => j)){
+    count++;
+  }
+  if(isSightlineOccupied(i, j, currentSeatsOccupied, (i) => i + 1, (j) => j + 1)){
+    count++;
+  }
+
+  return count;
+}
+
+export default function (input: string): string {
+  let currentSeatsOccupied: Array<Array<String>>;
+  let nextSeatsOccupied = input.split('\n').map(row => row.split(''));
+
+  do {
+    currentSeatsOccupied = JSON.parse(JSON.stringify(nextSeatsOccupied))
+
+    for(let i = 0; i < currentSeatsOccupied.length; i++){
+      for(let j = 0; j < currentSeatsOccupied[i].length; j++){
+        switch (currentSeatsOccupied[i][j]){
+          case '.':
+            break;
+          case 'L':
+            if(countAdjacentOccupiedSeats(i, j, currentSeatsOccupied) === 0){
+              nextSeatsOccupied[i][j] = '#';
+            }
+            break;
+          case '#':
+            if(countAdjacentOccupiedSeats(i, j, currentSeatsOccupied) >= 5){
+              nextSeatsOccupied[i][j] = 'L'
+            }
+            break;
+        }
+      }
+    }
+
+  } while (JSON.stringify(currentSeatsOccupied) !== JSON.stringify(nextSeatsOccupied))
+
+  // Now that we have a steady state, count occupied seats. 
+  let finalCount = 0;
+
+  for(let i = 0; i < nextSeatsOccupied.length; i++){
+    for(let j = 0; j < nextSeatsOccupied[i].length; j++){
+      if (nextSeatsOccupied[i][j] === '#'){
+        finalCount++;
+      }
+    }
+  }
+
+  return (finalCount).toString();
 }
